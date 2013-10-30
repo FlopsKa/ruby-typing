21be5b9757901283a548de103345ad2bbe746b1f
FlopsKa <flops.ka@gmail.com>
2013-10-30 12:20:55 +0100

    move the task upwards when end of line

--- public/js/words.js
+++ public/js/words.js
@@ -18,19 +18,16 @@ var allWords = {
 };
 
 var task = {
-  current_word: 0,
-
   show: function() {
     var output = "";
     for(var i = 0; i < allWords.words.length; i++) {
       output += "<span class=\"\" wordid=\"" + allWords.words[i].id + "\" wordnum=\"" + i + "\">" + allWords.words[i].word + "</span> ";
-      if((i + 1) % 7 == 0 && i > 0) output += "<br />";
     }
     $('#speedtest').html("<div class=\"task\">" + output + "</div>");
     $('#speedtest').append("<div class=\"word_input\">" +
         "<form class=\"input-group\"><input onkeypress=\"setTimeout(task.input, 0)\" autofocus class=\"form-control speedtest_input\" /><span class=\"timer input-group-addon\">00</span>" +
         "</form></div>");
-    $('span[wordnum=' + task.current_word + ']').addClass('currentword');
+    $('span[wordnum=0]').addClass('currentword');
   },
   input: function() {
     var input_field = $('.speedtest_input');
@@ -39,13 +36,22 @@ var task = {
     // go to next word on <space>
     if(input.slice(-1) == ' ') {
       input_field.val('');
-      task.current_word++;
       task.update();
     }
   },
   update: function() {
-    $('.currentword').removeClass('currentword');
-    $('span[wordnum=' + task.current_word + ']').addClass('currentword');
+    var current_old = $('.currentword');
+    current_old.removeClass('currentword');
+    var current = current_old.next('span');
+    current.addClass('currentword');
+
+    // move the lines upwards when the cursor jumps to the next line
+    if(current_old.offset().top != current.offset().top) {
+      // delete the previous spans
+      for(var i = 0; i <= current_old.attr("wordnum"); i++) {
+        $("[wordnum='" + i + "']").remove();
+      }
+    }
   }
 };
 
