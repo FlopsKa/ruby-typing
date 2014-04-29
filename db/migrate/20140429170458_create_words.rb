798458a7b80d9a30ac1b95f50489a0f0f15c369c
flopska <flops.ka@gmail.com>
2014-04-29 20:35:07 +0200

    automatically insert words when creating the table

--- db/migrate/20140429170458_create_words.rb
+++ db/migrate/20140429170458_create_words.rb
@@ -1,11 +1,15 @@
 class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
-      t.string :word
+      t.string :text
       t.integer :frequency
       t.integer :mistakes
 
       t.timestamps
+    end
+    File.open("all_words.txt").each do |w|
+      puts w
+      Word.create(text: w.chop, frequency: 0, mistakes: 0)
     end
   end
 end
